22375e24543ca9d0b004244325be8291a7313c57
rawnaqul <slow.coach.rawnaq@gmail.com>
2022-11-08 17:30:14 +0600

    APP.GET added

--- index.js
+++ index.js
@@ -26,54 +26,24 @@ console.log(uri);
 
 async function run() {
     try {
-        const userCollection = client.db('cateringServices').collection('users');
+        const serviceCollection = client.db('cateringServices').collection('services');
 
-        app.get('/users', async (req, res) => {
+        app.get('/services', async (req, res) => {
             const query = {};
-            const cursor = userCollection.find(query);
-            const users = await cursor.toArray();
-            res.send(users);
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
         });
 
-        app.get('/users/:id', async (req, res) => {
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
+            console.log(id);
             const query = { _id: ObjectId(id) };
-            const user = await userCollection.findOne(query);
-            res.send(user);
-        })
-
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const result = await userCollection.insertOne(user)
-            res.send(result)
+            const services = await serviceCollection.findOne(query);
+            res.send(services);
         });
 
-        app.put('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const user = req.body;
-            const option = { upsert: true };
-            const updatedUser = {
-                $set: {
-                    name: user.name,
-                    address: user.address,
-                    email: user.email
-                }
-            }
-            const result = await userCollection.updateOne(filter, updatedUser, option);
-            res.send(result);
-        })
 
-        app.delete('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            // console.log('Trying to delete', id);
-            const query = { _id: ObjectId(id) }
-            const result = await userCollection.deleteOne(query);
-            console.log(result);
-            res.send(result);
-
-        })
     }
 
     finally {
