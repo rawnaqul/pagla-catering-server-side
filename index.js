dd90f6ff4fac95e32bf37aef80cfc01786b76551
rawnaqul <slow.coach.rawnaq@gmail.com>
2022-11-10 21:58:40 +0600

    done

--- index.js
+++ index.js
@@ -105,6 +105,63 @@ async function run() {
             res.send(purchase);
         });
 
+        //=====MORE REVIEW OPS=====
+
+        // Review get by id
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await reviewCollection.findOne(query);
+            res.send(user);
+        })
+
+        // Review get in user page
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
+        });
+
+        // Review Delete=================================
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        // Review Update ======================
+        app.patch('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const newReview = req.body;
+            console.log(newReview);
+            const option = { upsert: true };
+            const updatedReview = {
+                $set: {
+                    reviewMessage: newReview.reviewMessage,
+                }
+            }
+            const result = await reviewCollection.updateOne(filter, updatedReview, option);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
         // Dashboard
         app.patch('/purchase/:id', async (req, res) => {
             const id = req.params.id;
